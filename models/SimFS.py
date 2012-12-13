893623fa9d2e93b0a99df3e0aa3797109e8f7e2a
markkampe <mark.kampe@inktank.com>
2012-12-12 18:24:53 -0800

    models/SimFS ... inode updates and variable extents    it now seems to crudely model btrfs performance    I'll have to look at different fs soon
    Signed-off-by: markkampe <mark.kampe@inktank.com>

--- models/SimFS.py
+++ models/SimFS.py
@@ -10,8 +10,10 @@ class SimFS:
     """ Performance Modeling File System Simulation. """
 
     # basic file system performance parameters
-    inode_sep = 0.9
-    index_sep = 0.1
+    inode_sep = 0.9     # distance from data to inode blocks
+    index_sep = 0.1     # distance from data to index blocks
+    max_extent = 32 * 1024    # largest variable extent
+    sync_time = 1000000     # inode update interval (us)
     pointer_length = 8
 
     def __init__(self, disk, extent_size=4096):
@@ -30,26 +32,34 @@ class SimFS:
 
         # figure out how many extents this operation involves
         num_ptr = self.extent / self.pointer_length
-        num_extents = float(bsize) / self.extent
-
-        if not read:
-            # how many new index blocks will we use in this operation
-            num_xblocks = num_extents / num_ptr
-
-            # figure out how lont it takes to update an index block
-            index_seek = \
-                self.disk.seekTime(self.disk.cylinders * self.index_sep)
-            index_latency = \
-                self.disk.latency(self.extent, read=False, seq=False, depth=1)
-            index_write = self.disk.xferTime(self.extent, read=False)
-
-            if sync:
-                # write out new index blocks PLUS the pointers to them
-                num_xwrites = 1 + num_xblocks
+        if self.max_extent > self.extent:
+            if bsize < self.max_extent:
+                num_extents = 1
             else:
-                # only write out new index blocks
-                num_xwrites = num_xblocks
+                num_extents = float(bsize) / self.max_extent
+        else:
+            num_extents = float(bsize) / self.extent
 
-            time += num_xwrites * (index_seek + index_latency + index_write)
+        # how long does it take to consult/update the index
+        num_xblocks = num_extents / num_ptr
+        inode_seek = \
+            self.disk.seekTime(self.disk.cylinders * self.inode_sep)
+        index_seek = \
+            self.disk.seekTime(self.disk.cylinders * self.index_sep)
+        index_latency = \
+            self.disk.latency(self.extent, read=read, seq=False, depth=1)
+        index_xfer = self.disk.xferTime(self.extent, read=read)
+
+        if read:
+            if not seq:
+                # still have to read index blocks to find our data
+                time += num_xblocks * (index_seek + index_latency + index_xfer)
+        else:
+            # we have to update a bunch of index blocks
+            time += num_xblocks * (index_seek + index_latency + index_xfer)
+
+            # and we occasionally have to update an inode
+            inode_upd = 1 if sync else time / float(self.sync_time)
+            time += inode_upd * (inode_seek + index_latency + index_xfer)
 
         return time

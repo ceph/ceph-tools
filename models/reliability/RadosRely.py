134a9592b1379b10a4449d9d03c82df8a88aa8b7
Mark Kampe <Mark.Kampe@gmail.com>
2012-12-29 16:42:36 -0800

    rados reliability model - correct expected loss
    1. we expect to lose only 1/2 of a PG as a result of a drive failure
    during recovery
    2. made code handling many-to-many recovery more obvious

--- models/reliability/RadosRely.py
+++ models/reliability/RadosRely.py
@@ -6,11 +6,12 @@ import RelyFuncts
 
 MB = 1000000     # unit for recovery speed
 
+
 class RADOS:
-    """ model a RADOS pool """
+    """ model a single-volume RADOS OSD """
 
     def __init__(self, disk, pg=200, copies=1, speed=50 * MB, delay=0):
-        """ create a RAID reliability simulation
+        """ create a RADOS reliability simulation
             pg -- number of placement groups per OSD
             copies -- number of copies for these objects
             speed -- expected recovery rate (bytes/second)
@@ -23,25 +24,25 @@ class RADOS:
         self.delay = delay
         self.description = "RADOS: %d cp, %d pg" % (copies, pg)
 
+    def rebuild_time(self):
+        seconds = self.disk.size / (self.speed * self.pgs)
+        return float(seconds * RelyFuncts.HOUR) / (60 * 60)
+
     def p_failure(self, period=RelyFuncts.YEAR):
         """ probability of data loss during a period """
-   
+
         # probability of an initial disk failure
         p_fail = self.disk.p_failure(period=period)
 
-        # probability of further failures during re-silvering
-        #   
-        #   Note that parallel recovery is (to a first approximation)
-        #   a reliability wash, as the reduced recovery time is (almost
-        #   exactly) compensated for by the increased number of volumes
-        #   whose failures can impact the recovery
-        s_recover = self.disk.size / self.speed
-        h_recover = float(self.delay) + (s_recover / (60 * 60))
-        p_fail2 = self.disk.p_failure(period=h_recover)
-        
+        # probability of another volume failure during recovery
+        recover = float(self.delay) + self.rebuild_time()
+        p_fail2 = self.disk.p_failure(period=recover)
+
+        # note that declustering increases the number of volumes
+        # upon which we depend
         copies = self.copies - 1
         while copies > 0:
-            p_fail *= p_fail2
+            p_fail *= (p_fail2 * self.pgs)
             copies -= 1
 
         return p_fail
@@ -49,8 +50,11 @@ class RADOS:
     def loss(self):
         """ amount of data lost after a drive failure """
 
-        # NOTE: this assumes perfect declustering, which is
-        #   untrue if the number of placement groups per OSD
-        #   is greater than the number of OSDs
-        return self.disk.size / self.pgs
-
+        # with perfect declustering (which requires more OSDs
+        # than placement groups/osd) we should expect to lose
+        # about 1/2 of a placement group as a result of a second
+        # drive failure.
+        l = self.disk.size
+        if self.copies > 1:
+            l /= (2 * self.pgs)
+        return l

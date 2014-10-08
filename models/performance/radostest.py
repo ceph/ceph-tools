3817a8a284dd2770b34bec79d22f421dc8ed0238
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2014-10-08 15:53:52 +0200

    performance/radostest.py: remove unnecessary parens after 'print'
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- models/performance/radostest.py
+++ models/performance/radostest.py
@@ -31,12 +31,12 @@ def radostest(fs, obj_size=16 * MILLION, nobj=2500,
     if crtdlt:
         tc = fs.create(depth=depth)
         td = fs.delete(depth=depth)
-        print("\t\t     create\t      delete")
-        print("\t\t%6d IOPS\t %6d IOPS" % (iops(tc), iops(td)))
-        print("")
+        print "\t\t     create\t      delete"
+        print "\t\t%6d IOPS\t %6d IOPS" % (iops(tc), iops(td))
+        print ""
 
-    print("\t    bs\t    rnd read\t   rnd write")
-    print("\t -----\t    --------\t   ---------")
+    print "\t    bs\t    rnd read\t   rnd write"
+    print "\t -----\t    --------\t   ---------"
     for bs in (4096, 128 * 1024, 4096 * 1024):
         trr = fs.read(bs, obj_size, nobj=nobj, clients=clients, depth=depth)
         trw = fs.write(bs, obj_size, nobj=nobj, depth=depth,
@@ -45,4 +45,4 @@ def radostest(fs, obj_size=16 * MILLION, nobj=2500,
         format = "\t%5dK\t%7.1f MB/s\t%7.1f MB/s"
         print(format %
               (kb(bs), bw(bs, float(trr)), bw(bs, float(trw))))
-        print("\t    \t %6d IOPS\t %6d IOPS" % (iops(trr), iops(trw)))
+        print "\t    \t %6d IOPS\t %6d IOPS" % (iops(trr), iops(trw))

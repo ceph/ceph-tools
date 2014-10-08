0b455241426f01fdd894e9e4dae8f9e0202dd923
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2014-10-08 15:49:43 +0200

    performance/fstest.py: fix wrong continued indentation
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- models/performance/fstest.py
+++ models/performance/fstest.py
@@ -26,7 +26,7 @@ def bw(bs, us):
 
 
 def fstest(fs, filesize=16 * MILLION, depth=1, direct=False,
-            sync=False, crtdlt=False):
+           sync=False, crtdlt=False):
     """ compute & display standard fio to filesystem on a disk
         fs -- file system to be tested
         filesize -- size of file in which I/O is being done
@@ -47,17 +47,17 @@ def fstest(fs, filesize=16 * MILLION, depth=1, direct=False,
     for bs in (4096, 128 * 1024, 4096 * 1024):
         tsr = fs.read(bs, filesize, seq=True, depth=depth, direct=direct)
         tsw = fs.write(bs, filesize, seq=True, depth=depth, direct=direct,
-                    sync=sync)
+                       sync=sync)
         trr = fs.read(bs, filesize, seq=False, depth=depth, direct=direct)
         trw = fs.write(bs, filesize, seq=False, depth=depth, direct=direct,
-                    sync=sync)
+                       sync=sync)
 
         if bw(bs, tsw) >= 10:
             format = "\t%5dK\t%7d MB/s\t%7d MB/s\t%7.1f MB/s\t%7.1f MB/s"
         else:
             format = "\t%5dK\t%7.1f MB/s\t%7.1f MB/s\t%7.1f MB/s\t%7.1f MB/s"
         print(format %
-                (kb(bs), bw(bs, tsr), bw(bs, tsw),
-                 bw(bs, float(trr)), bw(bs, float(trw))))
+              (kb(bs), bw(bs, tsr), bw(bs, tsw),
+               bw(bs, float(trr)), bw(bs, float(trw))))
         print("\t    \t %6d IOPS\t %6d IOPS\t %6d IOPS\t %6d IOPS" %
-                (iops(tsr), iops(tsw), iops(trr), iops(trw)))
+              (iops(tsr), iops(tsw), iops(trr), iops(trw)))

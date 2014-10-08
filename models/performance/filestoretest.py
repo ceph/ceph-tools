40e8c9add24c5b1eb1c516483b776ae37de27781
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2014-10-08 15:47:32 +0200

    filestoretest.py: remove unnecessary parens after 'print'
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- models/performance/filestoretest.py
+++ models/performance/filestoretest.py
@@ -29,12 +29,12 @@ def fstoretest(fs, obj_size=4 * MILLION, nobj=2500, depth=1, crtdlt=False):
     if crtdlt:
         tc = fs.create()
         td = fs.delete()
-        print("\t\t     create\t      delete")
-        print("\t\t%6d IOPS\t %6d IOPS" % (iops(tc), iops(td)))
-        print("")
+        print "\t\t     create\t      delete"
+        print "\t\t%6d IOPS\t %6d IOPS" % (iops(tc), iops(td))
+        print ""
 
-    print("\t    bs\t    rnd read\t   rnd write")
-    print("\t -----\t    --------\t   ---------")
+    print "\t    bs\t    rnd read\t   rnd write"
+    print "\t -----\t    --------\t   ---------"
     for bs in (4096, 128 * 1024, 4096 * 1024):
         trr = fs.read(bs, obj_size, depth=1, nobj=nobj)
         trw = fs.write(bs, obj_size, depth=depth, nobj=nobj)
@@ -42,4 +42,4 @@ def fstoretest(fs, obj_size=4 * MILLION, nobj=2500, depth=1, crtdlt=False):
         format = "\t%5dK\t%7.1f MB/s\t%7.1f MB/s"
         print(format %
               (kb(bs), bw(bs, float(trr)), bw(bs, float(trw))))
-        print("\t    \t %6d IOPS\t %6d IOPS" % (iops(trr), iops(trw)))
+        print "\t    \t %6d IOPS\t %6d IOPS" % (iops(trr), iops(trw))

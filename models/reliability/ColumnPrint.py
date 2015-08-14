00f259ff301860e0f1815b1d36512275ad5c1106
Mark.Kampe <mark.kampe@gmail.com>
2015-08-14 14:44:25 -0700

    Reliability model: enhanced formatted output functions 	moved the neatly formatted output stuff into a new class 	moved parameter printing into a separate routine (simplify run) 	changed run to use the new class and routine
    Signed-off-by: Mark.Kampe <mark.kampe@gmail.com>

---
+++ models/reliability/ColumnPrint.py
@@ -0,0 +1,134 @@
+#
+# This is a class to output model results (times, capacities, durabilities,
+# and probabilities) in attractive, standard width columns.
+#
+#   This class defines a standard string format for each line.
+#   It includes methods to turn many data types into strings.
+#   Create each output line as a vector of string (values),
+#   and then print it with the chosen format.
+#
+
+#
+# FIX push this factored-out pretty-printer back to ceph-tools
+#
+
+from RelyFuncts import YEAR, DAY, HOUR, MINUTE, SECOND
+
+
+class ColumnPrint:
+    """ a class to produce attractive columnar output """
+
+    def __init__(self, headings, maxdesc=20):
+        """ derive the format string """
+        self.headings = headings
+        Indent = 4
+        DescWid = maxdesc
+        ColWid = 12
+
+        # figure out how wide our columns have to be
+        wid = 0
+        for s in self.headings:
+            if len(s) > wid:
+                wid = len(s)
+        if wid >= ColWid:
+            ColWid = wid + 1
+
+        # generate the format string
+        f = ""
+        i = 0
+        while i < Indent:
+            f += ' '
+            i += 1
+
+        col = 0
+        while col < len(self.headings):
+            wid = DescWid if col == 0 else ColWid
+            f += '%'
+            if col == 0:
+                f += "-%ds" % wid
+            else:
+                f += "%ds" % wid
+            col += 1
+
+        self.format = f
+
+    def printLine(self, list):
+        """ print an output line from a list (of string items) """
+        print(self.format % tuple(list))
+
+    def printHeadings(self):
+        """ print out a set of column headings and separator line """
+        print ""
+        print self.format % self.headings
+
+        # how wide should a dash be
+        dashes = 0
+        for s in self.headings:
+            if len(s) > dashes:
+                dashes = len(s)
+
+        # create a line with that many dashes
+        s = ""
+        while dashes > 0:
+            s += '-'
+            dashes -= 1
+
+        # create a tupple with the right number of lines
+        l = list()
+        i = 0
+        while i < len(self.headings):
+            l.append(s)
+            i += 1
+
+        print self.format % tuple(l)
+
+    def printSize(self, sz, unit=1000):
+        """ print out a size with the appropriate unit suffix """
+
+        fmt10 = ["%dB", "%dKiB", "%dMiB", "%dGiB", "%dTiB", "%dPiB"]
+        fmt2 = ["%dB", "%dKB", "%dMB", "%dGB", "%dTB", "%dPB"]
+        fmt = fmt10 if unit == 1000 else fmt2
+        i = 0
+        while i < len(fmt):
+            if sz < unit:
+                break
+            sz /= unit
+            i += 1
+        return fmt[i] % (sz)
+
+    def printTime(self, t):
+        """ print out a time in an appropriate unit """
+        if t < 2 * MINUTE:
+            return "%d seconds" % (t / SECOND)
+        if t < 5 * HOUR:
+            return "%d minutes" % (t / MINUTE)
+        if t < 3 * DAY:
+            return "%d hours" % (t / HOUR)
+        if t < YEAR:
+            return "%d days" % (t / DAY)
+        if (t % YEAR) == 0:
+            return "%d years" % (t / YEAR)
+        else:
+            return "%5.1f years" % (t / YEAR)
+
+    def printDurability(self, d):
+        """ print out a durability in a reasonable format """
+        if d < .99999:
+            return "%6.3f%%" % (d * 100)
+        else:
+            nines = 0
+            while d > .9:
+                nines += 1
+                d -= .9
+                d *= 10
+            return "%d-nines" % (nines)
+
+    def printProbability(self, p):
+        """ print out a probability in a reasonable format """
+        if p > .0000001:
+            return "%9.6f%%" % (p * 100)
+        else:
+            return "%9.3e" % (p)
+
+    def printFloat(self, f):
+        return "%9.3e" % (f)
--- models/reliability/Run.py
+++ models/reliability/Run.py
@@ -9,130 +9,75 @@
 # Foundation.  See file COPYING.
 #
 
+from ColumnPrint import ColumnPrint
+from RelyFuncts import mttf, YEAR
+from sizes import PiB
+
 """
 run a series of tests
    This is the module that knows how to run a simulation,
    and how to report the results.
 """
 
-from RelyFuncts import SECOND, MINUTE, HOUR, DAY, YEAR, mttf
-from sizes import KiB, MiB, GiB, TiB, PiB
 
+def printParms(fmt, disk, raid, rados, site, multi):
+    """
+        print out the parameters associated with a test
+    """
+    if disk is not None:
+        print "Disk Modeling Parameters"
+        print "    size:     %10s" % fmt.printSize(disk.size)
+        print("    FIT rate: %10d (MTBF = %s)" %
+              (disk.fits, fmt.printTime(mttf(disk.fits))))
+        print "    NRE rate: %10.1E" % disk.nre
 
-def getFormat(headings):
-    """ derive the format string """
-    Indent = 4
-    DescWid = 20
-    ColWid = 12
+    if raid is not None:
+        print "RAID parameters"
+        print "    replace:  %16s" % fmt.printTime(raid.delay)
+        if raid.speed > 0:
+            print("    recovery rate: %7s/s (%s)" %
+                  (fmt.printSize(raid.speed),
+                   fmt.printTime(raid.rebuild_time())))
+        print "    NRE model:        %10s" % raid.nre_model
+        print "    object size:      %10s" % fmt.printSize(raid.objsize)
 
-    # figure out how wide our columns have to be
-    wid = 0
-    for s in headings:
-        if len(s) > wid:
-            wid = len(s)
-    if wid >= ColWid:
-        ColWid = wid + 1
+    if rados is not None:
+        print "RADOS parameters"
+        print "    auto mark-out: %14s" % fmt.printTime(rados.delay)
+        print("    recovery rate: %8s/s (%s/drive)" %
+              (fmt.printSize(rados.speed),
 
-    # generate the format string
-    f = ""
-    i = 0
-    while i < Indent:
-        f += ' '
-        i += 1
+               fmt.printTime(rados.rebuild_time(rados.speed))))
+        print "    osd fullness: %7d%%" % (rados.full * 100)
+        print "    declustering: %7d PG/OSD" % rados.pgs
+        print "    NRE model:        %10s" % rados.nre_model
+        print "    object size:  %7s" % \
+              fmt.printSize(rados.objsize, unit=1024)
+        print "    stripe length:%7d" % rados.stripe
 
-    col = 0
-    while col < len(headings):
-        wid = DescWid if col == 0 else ColWid
-        f += '%'
-        if col == 0:
-            f += "-%ds" % wid
+    if site is not None:
+        print "Site parameters"
+        s = 0 if multi is None else multi.sites
+        if site.fits == 0:
+            print "    disasters:    IGNORED"
         else:
-            f += "%ds" % wid
-        col += 1
-    return f
+            tf = mttf(site.fits)
+            print("    disaster rate: %12s (%d FITS)" %
+                  (fmt.printTime(tf), site.fits))
+        if site.replace == 0:
+            print "    site recovery:   NEVER"
+        else:
+            print "    site recovery: %11s" % fmt.printTime(site.replace)
 
-
-def printHeadings(headings, format):
-    """ print out a set of column headings """
-    print ""
-    print format % headings
-
-    # how wide should a dash be
-    dashes = 0
-    for s in headings:
-        if len(s) > dashes:
-            dashes = len(s)
-
-    # create a line with that many dashes
-    s = ""
-    while dashes > 0:
-        s += '-'
-        dashes -= 1
-
-    # create a tupple with the right number of lines
-    l = list()
-    i = 0
-    while i < len(headings):
-        l.append(s)
-        i += 1
-
-    print format % tuple(l)
-
-
-def printSize(sz, unit=1000):
-    """ print out a size with the appropriate unit suffix """
-
-    fmt10 = ["%dB", "%dKiB", "%dMiB", "%dGiB", "%dTiB", "%dPiB"]
-    fmt2 = ["%dB", "%dKB", "%dMB", "%dGB", "%dTB", "%dPB"]
-    fmt = fmt10 if unit == 1000 else fmt2
-    i = 0
-    while i < len(fmt):
-        if sz < unit:
-            break
-        sz /= unit
-        i += 1
-    return fmt[i] % (sz)
-
-
-def printTime(t):
-    """ print out a time in an appropriate unit """
-    if t < 2 * MINUTE:
-        return "%d seconds" % (t / SECOND)
-    if t < 5 * HOUR:
-        return "%d minutes" % (t / MINUTE)
-    if t < 3 * DAY:
-        return "%d hours" % (t / HOUR)
-    if t < YEAR:
-        return "%d days" % (t / DAY)
-    if (t % YEAR) == 0:
-        return "%d years" % (t / YEAR)
-    else:
-        return "%5.1f years" % (t / YEAR)
-
-
-def printDurability(d):
-    """ print out a durability in a reasonable format """
-    if d < .99999:
-        return "%6.3f%%" % (d * 100)
-    else:
-        nines = 0
-        while d > .9:
-            nines += 1
-            d -= .9
-            d *= 10
-        return "%d-nines" % (nines)
-
-
-def printProbability(p):
-    """ print out a probability in a reasonable format """
-    if p > .0000001:
-        return "%9.6f%%" % (p * 100)
-    else:
-        return "%9.3e" % (p)
-
-
-def printFloat(f):
-    return "%9.3e" % (f)
+        if multi is not None:
+            print("    recovery rate: %8s/s (%s/PG)" %
+                  (fmt.printSize(multi.speed),
+                   fmt.printTime(multi.rados.rebuild_time(multi.speed))))
+            if multi.latency == 0:
+                print "    replication:       synchronous"
+            else:
+                print("    replication:       asynchronous (%s delay)" %
+                      (fmt.printTime(multi.latency)))
 
 
 def Run(tests, period=YEAR, verbosity="all"):
@@ -142,6 +87,24 @@ def Run(tests, period=YEAR, verbosity="all"):
         period -- simulation period
         verbosity -- output options
     """
+
+    # column headings
+    heads = ("storage", "durability",
+             "PL(site)", "PL(copies)", "PL(NRE)", "PL(rep)", "loss/PiB")
+
+    # column descriptions
+    legends = [
+        "storage unit/configuration being modeled",
+        "probability of object survival*",
+        "probability of loss due to site failures*",
+        "probability of loss due to drive failures*",
+        "probability of loss due to NREs during recovery*",
+        "probability of loss due to replication failure*",
+        "expected data loss per Petabyte*"
+    ]
+
+    # use the headings to generate a format
+    format = ColumnPrint(heads, maxdesc=20)
 
     # figure out what output he wants
     headings = True
@@ -186,107 +149,41 @@ def Run(tests, period=YEAR, verbosity="all"):
     if disk is None and raid is not None:
         disk = raid.disk
 
-    if parms and disk is not None:
-        print "Disk Modeling Parameters"
-        print "    size:     %10s" % printSize(disk.size)
-        print("    FIT rate: %10d (MTBF = %s)" %
-              (disk.fits, printTime(mttf(disk.fits))))
-        print "    NRE rate: %10.1E" % disk.nre
-
-    if parms and raid is not None:
-        print "RAID parameters"
-        print "    replace:  %16s" % printTime(raid.delay)
-        if raid.speed > 0:
-            print("    recovery rate: %7s/s (%s)" %
-                  (printSize(raid.speed),
-                   printTime(raid.rebuild_time())))
-        print "    NRE model:        %10s" % raid.nre_model
-        print "    object size:      %10s" % printSize(raid.objsize)
-
-    if parms and rados is not None:
-        print "RADOS parameters"
-        print "    auto mark-out: %14s" % printTime(rados.delay)
-        print("    recovery rate: %8s/s (%s/drive)" %
-              (printSize(rados.speed),
-               printTime(rados.rebuild_time(rados.speed))))
-        print "    osd fullness: %7d%%" % (rados.full * 100)
-        print "    declustering: %7d PG/OSD" % rados.pgs
-        print "    NRE model:        %10s" % rados.nre_model
-        print "    object size:  %7s" % printSize(rados.objsize, unit=1024)
-        print "    stripe length:%7d" % rados.stripe
-
-    if parms and site is not None:
-        print "Site parameters"
-        s = 0 if multi is None else multi.sites
-        if site.fits == 0:
-            print "    disasters:    IGNORED"
-        else:
-            tf = mttf(site.fits)
-            print("    disaster rate: %12s (%d FITS)" %
-                  (printTime(tf), site.fits))
-        if site.replace == 0:
-            print "    site recovery:   NEVER"
-        else:
-            print "    site recovery: %11s" % printTime(site.replace)
-
-        if multi is not None:
-            print("    recovery rate: %8s/s (%s/PG)" %
-                  (printSize(multi.speed),
-                   printTime(multi.rados.rebuild_time(multi.speed))))
-            if multi.latency == 0:
-                print "    replication:       synchronous"
-            else:
-                print("    replication:       asynchronous (%s delay)" %
-                      (printTime(multi.latency)))
-
-    # column headings
-    heads = ("storage", "durability",
-             "PL(site)", "PL(copies)", "PL(NRE)", "PL(rep)", "loss/PiB")
-    format = getFormat(heads)
-
-    # column descriptions
-    legends = [
-        "storage unit/configuration being modeled",
-        "probability of object survival",
-        "probability of loss due to site failures",
-        "probability of loss due to drive failures",
-        "probability of loss due to NREs during recovery",
-        "probability of loss due to replication failure",
-        "expected data loss per Petabyte"
-    ]
+    if parms:
+        printParms(format, disk, raid, rados, site, multi)
 
     if descr:
         print ""
         print "Column legends"
-        s = printTime(period)
+        s = format.printTime(period)
         i = 1
         while i <= len(legends):
             l = legends[i - 1]
-            if i == 1:
-                print "\t%d %s" % (i, l)
-            else:
+            if l.endswith('*'):
                 print "\t%d %s (per %s)" % (i, l, s)
+            else:
+                print "\t%d %s" % (i, l)
             i += 1
 
     if headings:
-        printHeadings(heads, format)
+        format.printHeadings()
 
     # expected data loss after drive failures
     for t in tests:
         if t is None:
-            printHeadings(heads, format)
+            format.printHeadings()
             continue
 
         # calculate the renderable reliabilities and durability
         s = list()
         t.compute(period=period)
         s.append(t.description)                 # description
-        s.append(printDurability(t.dur))        # durability
-        s.append(printProbability(t.P_site))    # P(site failure)
-        s.append(printProbability(t.P_drive))   # P(drive failure)
-        s.append(printProbability(t.P_nre))     # P(NRE on recovery)
-        s.append(printProbability(t.P_rep))     # P(replication failure)
+        s.append(format.printDurability(t.dur))        # durability
+        s.append(format.printProbability(t.P_site))    # P(site failure)
+        s.append(format.printProbability(t.P_drive))   # P(drive failure)
+        s.append(format.printProbability(t.P_nre))     # P(NRE on recovery)
+        s.append(format.printProbability(t.P_rep))     # P(replication failure)
         l = (t.P_site * t.L_site) + (t.P_drive * t.L_drive) +\
             (t.P_nre * t.L_nre) + (t.P_rep * t.L_rep)
-        s.append(printFloat(l * PiB / t.rawsize))   # expected data loss/PiB
-        print format % tuple(s)
+        s.append(format.printFloat(l * PiB / t.rawsize))   # expected loss/PiB
+        format.printLine(s)

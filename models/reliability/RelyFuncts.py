0d0c6dd6f2b9f28a48104aea7c6c7e7b8103be2e
Mark.Kampe <mark.kampe@gmail.com>
2015-08-14 08:50:15 -0700

    Reliability Model: new reliability function 	multiFit computes effective FIT rate of n out of m components
    Signed-off-by: Mark.Kampe <mark.kampe@gmail.com>

--- models/reliability/RelyFuncts.py
+++ models/reliability/RelyFuncts.py
@@ -26,15 +26,22 @@ SECOND = float(HOUR) / 3600
 DAY = float(HOUR * 24)
 YEAR = (HOUR * 24 * 365.25)
 
+BILLION = 1000000000
+
 
 def FitRate(events, period=YEAR):
-    """ return the FIT rate corresponding to a rate in other unit"""
-    return events * 1000000000 / period
+    """ FIT rate corresponding to a rate in other unit
+            events -- number of events
+            period --  period in which that many events happen
+    """
+    return events * BILLION / period
 
 
 def mttf(fits):
-    """ return the MTTF corresponding to an FIT rate """
-    return 1000000000 / fits
+    """ MTTF corresponding to an FIT rate
+            fits --- FIT rate
+    """
+    return BILLION / fits
 
 
 def Pfail(fitRate, hours, n=1):
@@ -62,3 +69,35 @@ def Pn(expected=1, n=0):
         p *= (expected ** n)
         p /= math.factorial(n)
     return p
+
+
+def multiFit(fitRate, total, required, repair, oneRepair=True):
+    """ effective FIT rate required/total redundant components
+            fitRate -- FIT rate of a single component
+            total -- number of redundant components in system
+            required -- number required for continued operation
+            repair -- repair time (in hours)
+            oneRepair -- all failures within a single repair period
+
+        FITs(all_fail) =
+            FITs(initial failure) * P(rest fail during repair period)
+    """
+
+    # FIX ... these are only approximations, I should do better
+    fits = total * fitRate      # initial FIT rate
+    total -= 1                  # we are down one
+    if oneRepair:
+        # all failures must occur within a single repair period
+        # note: this very slightly under-estimates P_restfail in cases
+        #       where required > 1
+        P_restfail = Pfail(total*fitRate, repair, n=total+1-required)
+        fits *= P_restfail
+    else:
+        # each failure starts a new repair period
+        # note: these numbers are small enough that expected reasonably
+        #       approximates the probability
+        while total >= required:
+            P_nextfail = total * fitRate * repair * 10E-9
+            fits *= P_nextfail
+            total -= 1
+    return fits
